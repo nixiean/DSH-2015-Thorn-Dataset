adf08c1c2c43c3b15999ca6846bb74b0bd63eeae
Nikhil <nikhilnavali@gmail.com>
2015-03-07 19:39:38 -0500

    Commit

--- dsh2015.py
+++ dsh2015.py
@@ -6,10 +6,7 @@ import sys
 
 csv.field_size_limit(sys.maxsize)
 
-inputFile = 'escort_all.tsv'
-
-outputFileStates = 'states.tsv'
-outputFileCities = 'cities.tsv'
+inputFile = 'states.tsv'
 
 statesUSA = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 states = {}
@@ -18,23 +15,15 @@ location = set()
 # Function to read the data
 def read_data(data):
     i = 0
-    fOutputStates = csv.writer(open(outputFileStates,"r+"), dialect="excel-tab") 
-    fOutputCities = csv.writer(open(outputFileCities,"r+"), dialect="excel-tab")
     fInput = csv.reader(open(inputFile,"rU"), dialect="excel-tab")
     for line in fInput:
         i = i+1
         print i
-        if len(line) > 1:
-            if (line[1] in statesUSA):
-                if states.get(line[1]):
-                    states[line[1]] = states[line[1]] + 1 
-                else:
-                    states[line[1]] = 1;
-                fOutputStates.writerow(line)
-            else:
-                fOutputCities.writerow(line)
-        else: 
-            fOutputCities.writerow(line)
+        print line
+        #if len(line) > 1:
+        #    if (line[1] in statesUSA):
+        #            states[line[1]] = states[line[1]] + 1 
+              
     print states         
     
 def main():
